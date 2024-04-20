d3a0ebbb6f03ddb39b00cf105e0d67d95899faf8
lukasKuky <167614128+lukasKuky@users.noreply.github.com>
2024-04-20 14:24:31 +0200

    first commit

---
+++ src/main/java/com/example/ISS/IssApplication.java
@@ -0,0 +1,13 @@
+package com.example.ISS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ISS/IssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ISS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
